34b19e23bf21e3290fd4b66723036327eea9d0f2
kara <ssctonioloriccardo@gmail.com>
2022-07-10 15:30:12 +0200

    experiments with astronvim + added env variables for the node project.

--- Node-assignment1/app.js
+++ Node-assignment1/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const routing = require('./routes');
 
-const Server = http.createServer(routing);
+    const Server = http.createServer(routing);
 Server.listen(3000);
