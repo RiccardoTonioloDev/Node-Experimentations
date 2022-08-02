b63f5c13d7fdf99c11f8d9d55b0281a1edba3adb
kara <ssctonioloriccardo@gmail.com>
2022-08-02 18:48:16 +0200

    Added a REACT front-end to test the APIs, and learned about CORS errors.

--- Node-first-RESTAPI/app.js
+++ Node-first-RESTAPI/app.js
@@ -8,6 +8,24 @@ const app = express();
 //Non useremo .urlencoded() poichè riceviamo JSON (è cambiato il content type delle richieste)
 app.use(bodyParser.json());
 
+//Ora ogni risposta che manderemo avrà questi headers, e di conseguenza
+//non genererà più errori di tipo CORS.
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	//Per specificare da che dominio possiamo ricevere delle richieste.
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH'
+	);
+	//Per specificare che tipo di richieste possiamo ricevere.
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Content-Type, Authorization'
+	);
+	//Per specificare che tipi di headers possono essere settati nella richiesta.
+	next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
